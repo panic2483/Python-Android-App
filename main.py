54f319f0619fcb350f4785cfabbef1773bd6170d
Patrick <patrick.m.fricke@gmx.de>
2025-11-20 16:35:42 +0100

    restart

--- main.py
+++ main.py
@@ -1,55 +1,13 @@
-# Kivy Imports
+import kivy
+kivy.require("2.3.1")
+
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout 
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.core.window import Window
+from kivy.uix.label import Label
 
-Window.size = (500,600) #Fenster Größe 500x600
-
-class Calculator(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(orientation = "vertical", **kwargs)
-
-        self.result = TextInput(
-            font_size = 45,
-            size_hint_y = 0.2,
-            readonly = True,
-            halign = "right",
-            multiline = False
-        )
-
-        self.add_widget(self.result)
-        
-        #Buttons der App, jede Reihe = Reihe in der App
-        buttons = [
-            ["C", "+/-", "%", "/"],
-            ["7", "8", "9", "*"],
-            ["4", "5", "6", "-"],
-            ["1", "2","3", "+"],
-            ["0", "00", ".", "="]
-        ]
-
-        grid = GridLayout(cols = 4, spacing = 5, padding = 10) #Cols = 4 = 4 Reihen im Grid
-        for row in buttons: #Fügt automatisch die Texte aus "buttons" den Buttons der App hinzu
-            for item in row: 
-                button = Button(
-                    text = item,
-                    font_size = 32,
-
-                )
-                grid.add_widget(button) #Erstellt die Buttons
-        
-        self.add_widget(grid) #Erstellt das Grid (mit den Buttons)
-
-
-class CalculatorApp(App):
+class FirstApp(App):
     def build(self):
-        return Calculator()
+        return Label(text = "Hello World!")
     
-
 if __name__ == "__main__":
-    CalculatorApp().run()
-
+    FirstApp().run()
 
